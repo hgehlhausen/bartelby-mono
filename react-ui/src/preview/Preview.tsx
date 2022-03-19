835fe5cde84860c1ab16ba01d15b506f3b939c22
Heath Gehlhausen <heath.gehlhausen@gmail.com>
2022-03-19 14:55:51 -0500

    correcting print-preview
    - ensure that printing does not include controls
    - ensure that printing does not overly shift cards
    - adjust print-coloring to avoid black background

--- react-ui/src/preview/Preview.tsx
+++ react-ui/src/preview/Preview.tsx
@@ -2,13 +2,26 @@ import React from 'react';
 import { selectCardQueue } from '../store/selectors';
 import Card, { CardProps } from '../card/Card';
 import { useSelector } from '../store/store'
+
 import './Preview.css';
 
 interface PreviewProps {}
 
 export default function Preview(props: PreviewProps) {
   const queue = useSelector(selectCardQueue);
-  return <div className="Preview">
-    {queue.map((data: CardProps) => <Card {...data} removable />)}
-  </div>;
+  
+  function print3x3(cards: Array<CardProps>) {
+    const pages = [];
+    for( let i = 0; i< cards.length; i+=9) {
+      let children = cards.slice(i, i+9);
+      pages.push(<div className="page">
+        {children.map((data: CardProps) => <Card {...data} removable />)}
+      </div>);
+    }
+    return pages;
+  }
+
+  return (<div className="Preview">
+    {print3x3(queue)}
+  </div>);
 }
